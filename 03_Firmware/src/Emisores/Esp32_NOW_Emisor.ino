4f3d563ed3bca9def324a92e122f71b853a2a791
LeidisHoyosuq <leidisy.hoyosm@uqvirtual.edu.co>
2026-04-11 20:36:48 -0500

    Implementar la medición de distancia y la visualización
    Se agregó la funcionalidad de medición y visualización de distancia mediante la biblioteca U8g2. Se actualizó la estructura de mensajes para incluir la detección de obstáculos.

--- 03_Firmware/src/Emisores/Esp32_NOW_Emisor.ino
+++ 03_Firmware/src/Emisores/Esp32_NOW_Emisor.ino
@@ -1,12 +1,21 @@
 #include "ESP32_NOW.h"
 #include "WiFi.h"
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+
+// Configuración de pantalla para tu modelo (Pines 6 y 5)
+U8G2_SSD1306_72X40_ER_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 6, /* data=*/ 5);
 
 #define CHANNEL 6
-#define NODE_ID 1   // Cambiar en cada nodo
+#define NODE_ID 1 
+
+const int echoPin = 1; 
+const int trigPin = 2; 
 
 typedef struct {
   uint8_t id;
-  float valor;
+  char estado[20];
 } mensaje_t;
 
 mensaje_t mensaje;
@@ -17,10 +26,7 @@ public:
   : ESP_NOW_Peer(ESP_NOW.BROADCAST_ADDR, channel, iface, nullptr) {}
 
   bool begin() {
-    if (!ESP_NOW.begin() || !add()) {
-      Serial.println("Error iniciando ESP-NOW");
-      return false;
-    }
+    if (!ESP_NOW.begin() || !add()) return false;
     return true;
   }
 
@@ -33,25 +39,55 @@ BroadcastPeer peer(CHANNEL, WIFI_IF_STA);
 
 void setup() {
   Serial.begin(115200);
+  u8g2.begin();
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 
   WiFi.mode(WIFI_STA);
   WiFi.setChannel(CHANNEL);
 
   if (!peer.begin()) {
-    Serial.println("Fallo crítico");
-    while(true);
+    while(true) {
+        Serial.println("Error ESP-NOW");
+        delay(1000);
+    }
   }
-
-  Serial.println("Nodo listo");
 }
 
 void loop() {
+  // Medir distancia
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
-  mensaje.id = NODE_ID;
-  mensaje.valor = random(0,100);
+  long duracion = pulseIn(echoPin, HIGH);
+  float distancia = duracion * 0.034 / 2;
 
-  peer.sendData((uint8_t*)&mensaje, sizeof(mensaje));
-  Serial.println(mensaje.id);
-  Serial.println(mensaje.valor);
-  delay(1000);
+  // Mostrar en pantalla
+  u8g2.clearBuffer();
+  u8g2.drawFrame(0, 0, 72, 40); 
+  u8g2.setFont(u8g2_font_6x10_tf);
+  u8g2.drawStr(5, 12, "DISTANCIA:");
+  
+  u8g2.setCursor(15, 30);
+  if (distancia > 400 || distancia < 2) {
+    u8g2.print("--- cm");
+  } else {
+    u8g2.print(distancia, 1);
+    u8g2.print(" cm");
+  }
+  u8g2.sendBuffer();
+
+  // Envío si hay obstáculo
+  if (distancia > 0 && distancia < 15.0) {
+    mensaje.id = NODE_ID;
+    strcpy(mensaje.estado, "mano detectado"); 
+    peer.sendData((uint8_t*)&mensaje, sizeof(mensaje));
+    delay(500); 
+  }
+
+  delay(100);
 }
